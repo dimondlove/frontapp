4c17a9e81f10af549f7a102a5633aaa9328a5614
Yuriy_Rudnev <rudnev2020@gmail.com>
2024-01-25 22:40:11 +0300

    Creating project

---
+++ src/main/java/ru/rudnev/spring/frontapp/FrontappApplication.java
@@ -0,0 +1,13 @@
+package ru.rudnev.spring.frontapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rudnev/spring/frontapp/FrontappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rudnev.spring.frontapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
